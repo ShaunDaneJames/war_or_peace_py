43725117c04d7f6e6c52bd584bc7d9fdef3ebb80
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 16:12:23 -0600

    Adds test for knowing percentage of high ranked cards

--- test/test_deck.py
+++ test/test_deck.py
@@ -36,3 +36,14 @@ def test_returns_high_ranking_cards():
     assert deck.high_ranking_cards()[0] == card1
     assert deck.high_ranking_cards()[1] == card3
     assert len(deck.high_ranking_cards()) == 2
+
+def test_knows_percent_of_high_ranked_cards():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    card4 = Card("Heart", "2", 2)
+    cards = [card1, card2, card3, card4]
+
+    deck = Deck(cards)
+
+    assert deck.percent_high_ranking() == 50.0
