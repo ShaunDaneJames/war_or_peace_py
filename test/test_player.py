9647e33d31d92ef3b041204b7288cb9639a82e67
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 16:39:42 -0600

    Adds deck with player init

--- test/test_player.py
+++ test/test_player.py
@@ -1,7 +1,17 @@
 import pytest
 from lib.player import *
+from lib.deck import *
+from lib.card import *
+
 
 def test_it_exists():
-    player = Player('Calvin')
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck(cards)
+    player = Player('Calvin', deck)
 
     assert player.name == 'Calvin'
+    assert len(player.deck.cards) == 3
