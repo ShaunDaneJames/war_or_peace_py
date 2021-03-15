06a651beb2a54038c34d115e930aac8816d4c666
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 16:48:55 -0600

    Adds test for has lost

--- test/test_player.py
+++ test/test_player.py
@@ -15,3 +15,23 @@ def test_it_exists():
 
     assert player.name == 'Calvin'
     assert len(player.deck.cards) == 3
+
+
+def test_knows_if_player_lost():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck(cards)
+    player = Player('Calvin', deck)
+
+    assert not player.has_lost()
+
+    player.deck.remove_card()
+    player.deck.remove_card()
+
+    assert not player.has_lost()
+
+    player.deck.remove_card()
+    assert player.has_lost()
