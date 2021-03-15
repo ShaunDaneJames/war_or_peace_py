470938e82c96faf04abeeed71e006b082fff1246
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 16:01:33 -0600

    Adds Iterator to find high ranking cards

--- lib/deck.py
+++ lib/deck.py
@@ -2,3 +2,13 @@ class Deck:
     def __init__(self, cards):
         self.cards = cards
 
+    def rank_of_card_at(self, index):
+        return self.cards[index].rank
+
+    def high_ranking_cards(self):
+        high_ranked_list = []
+        for card in self.cards:
+            if card.rank > 10:
+                high_ranked_list.append(card)
+        return high_ranked_list
+
