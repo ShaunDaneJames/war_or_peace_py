df72a9910239bd9946151a9181eb37423fb6f821
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-15 16:00:54 -0600

    Adds tests for returning high ranking cards

--- test/test_deck.py
+++ test/test_deck.py
@@ -2,6 +2,7 @@ import pytest
 from lib.deck import *
 from lib.card import *
 
+
 def test_it_exists():
     card1 = Card("diamond", "Queen", 12)
     card2 = Card("spade", "3", 3)
@@ -12,7 +13,8 @@ def test_it_exists():
 
     assert len(deck.cards) == 3
 
-def test_rank_of_card_at(index):
+
+def test_rank_of_card_at():
     card1 = Card("diamond", "Queen", 12)
     card2 = Card("spade", "3", 3)
     card3 = Card("Heart", "Ace", 14)
@@ -20,4 +22,17 @@ def test_rank_of_card_at(index):
 
     deck = Deck(cards)
 
-    assert rank_of_card_at(0)
+    assert deck.rank_of_card_at(0) == 12
+    assert deck.rank_of_card_at(2) == 14
+
+def test_returns_high_ranking_cards():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck(cards)
+
+    assert deck.high_ranking_cards()[0] == card1
+    assert deck.high_ranking_cards()[1] == card3
+    assert len(deck.high_ranking_cards()) == 2
