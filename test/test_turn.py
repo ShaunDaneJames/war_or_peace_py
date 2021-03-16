1c0ae424e42fb3c010f041c40dd3495d5dd940b9
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-16 15:58:37 -0600

    Adds test for determining winner of basic turn

--- test/test_turn.py
+++ test/test_turn.py
@@ -68,3 +68,20 @@ def test_knows_war_turn_type():
     turn = Turn(player1, player2)
 
     assert turn.type() == "MAD"
+
+def test_knows_winner_of_basic_turn():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    card4 = Card("spade", "7", 7)
+    cards1 = [card1, card2]
+    cards2 = [card3, card4]
+
+    deck1 = Deck(cards1)
+    deck2 = Deck(cards2)
+    player1 = Player('Calvin', deck1)
+    player2 = Player('Hobbes', deck2)
+    turn = Turn(player1, player2)
+
+    assert turn.type() == "basic"
+    assert turn.winner() == player2
