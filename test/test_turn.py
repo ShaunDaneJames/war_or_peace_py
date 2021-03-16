af0ca30289c19b53429de06c93325fbd45e191ad
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-16 15:15:42 -0600

    Adds test for war and MAD turn types

--- test/test_turn.py
+++ test/test_turn.py
@@ -21,7 +21,7 @@ def test_it_exists():
     assert turn.spoils == []
 
 
-def test_it_turn_type():
+def test_knows_basic_turn_type():
     card1 = Card("diamond", "Queen", 12)
     card2 = Card("spade", "3", 3)
     card3 = Card("Heart", "Ace", 14)
@@ -36,3 +36,35 @@ def test_it_turn_type():
     turn = Turn(player1, player2)
 
     assert turn.type() == "basic"
+
+def test_knows_war_turn_type():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Queen", 12)
+    card4 = Card("spade", "7", 7)
+    cards1 = [card1, card2]
+    cards2 = [card3, card4]
+
+    deck1 = Deck(cards1)
+    deck2 = Deck(cards2)
+    player1 = Player('Calvin', deck1)
+    player2 = Player('Hobbes', deck2)
+    turn = Turn(player1, player2)
+
+    assert turn.type() == "war"
+
+def test_knows_war_turn_type():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Queen", 12)
+    card4 = Card("Heart", "3", 3)
+    cards1 = [card1, card2]
+    cards2 = [card3, card4]
+
+    deck1 = Deck(cards1)
+    deck2 = Deck(cards2)
+    player1 = Player('Calvin', deck1)
+    player2 = Player('Hobbes', deck2)
+    turn = Turn(player1, player2)
+
+    assert turn.type() == "MAD"
