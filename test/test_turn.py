164684d270fc7b66e08e4810380dd5147534e9b2
Shaun James <ShaunDaneJames@Gmail.com>
2021-03-16 15:05:20 -0600

    Adds test for turn type

--- test/test_turn.py
+++ test/test_turn.py
@@ -19,3 +19,20 @@ def test_it_exists():
     assert turn.player1.name == 'Calvin'
     assert turn.player2.name == 'Hobbes'
     assert turn.spoils == []
+
+
+def test_it_turn_type():
+    card1 = Card("diamond", "Queen", 12)
+    card2 = Card("spade", "3", 3)
+    card3 = Card("Heart", "Ace", 14)
+    card4 = Card("spade", "7", 7)
+    cards1 = [card1, card2]
+    cards2 = [card3, card4]
+
+    deck1 = Deck(cards1)
+    deck2 = Deck(cards2)
+    player1 = Player('Calvin', deck1)
+    player2 = Player('Hobbes', deck2)
+    turn = Turn(player1, player2)
+
+    assert turn.type() == "basic"
